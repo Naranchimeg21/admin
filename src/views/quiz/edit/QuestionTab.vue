663c2838691bc626ff7dcd798583e063d46a2173
nerguibaatar.b <nerguibaatar.b@unitel.mn>
2022-04-14 15:12:46 +0800

    answer drag fixed

--- src/views/quiz/edit/QuestionTab.vue
+++ src/views/quiz/edit/QuestionTab.vue
@@ -22,42 +22,56 @@ export default {
     QuestionCard,
   },
   props: ['questionsArray'],
-  data: () => ({
-    open: false,
-    questions: [],
-  }),
+  data() {
+    return {
+      open: false,
+      questions: [],
+    }
+  },
   watch: {
+    questions() {
+      console.log(this.questions)
+    },
     questionsArray(newValue) {
       this.questions = newValue
     },
   },
   methods: {
     removeQuestion(question) {
-      for (let i = 0; i < state.questions.length; i++) {
-        if (state.questions[i] === question) {
-          state.questions.splice(i, 1)
+      for (let i = 0; i < this.questions.length; i++) {
+        if (this.questions[i] === question) {
+          this.questions.splice(i, 1)
 
-          if (state.questions.length > 1) {
-            for (let index = i; index < state.questions.length; index++) {
-              state.questions[index].order--
+          if (this.questions.length > 1) {
+            for (let index = i; index < this.questions.length; index++) {
+              this.questions[index].order--
             }
           }
         }
       }
     },
     addNewQuestion() {
-      let lastOrder = state.questions[state.questions.length - 1].order
+      let orderNumber = 0
 
-      state.questions.push({
+      if (this.questions.length > 0) {
+        orderNumber = this.questions[this.questions.length - 1].order + 1
+      }
+
+      this.questions.push({
         text: '',
         type: 'fill_in',
         options: [],
-        order: lastOrder + 1,
+        order: orderNumber,
       })
     },
+    print() {
+      console.log(this.questions)
+    },
   },
-  mounted: () => {
+  mounted() {
     let el = document.getElementById('order-option')
+
+    let that = this
 
     Sortable.create(el, {
       animation: 150,
@@ -65,29 +79,24 @@ export default {
       ghostClass: 'shadow-lg',
       swapThreshold: 0.4,
       onChange: function (evt) {
-        let temp = state.questions[evt.oldIndex]
+        let temp = that.questions[evt.oldIndex]
 
-        state.questions.splice(evt.oldIndex, 1)
-        state.questions.splice(evt.newIndex, 0, temp)
+        that.questions.splice(evt.oldIndex, 1)
+        that.questions.splice(evt.newIndex, 0, temp)
       },
     })
   },
 }
-
-// const orderedQuestions = computed(() => {
-//   return state.questions.sort((a, b) => {
-//     if (a.order < b.order) return -1
-//     if (a.order > b.order) return 1
-//     return 0
-//   })
-// })
 </script>
 
 <template>
   <div class="flex flex-col space-y-5">
+    <button type="button" class="py-2 px-5 bg-red-500" @click="print">
+      hewleh
+    </button>
     <ul id="order-option" class="space-y-2">
       <QuestionCard
-        v-for="question in orderedQuestions"
+        v-for="question in questions"
         v-bind:key="question"
         :question="question"
         @remove-question="removeQuestion"
